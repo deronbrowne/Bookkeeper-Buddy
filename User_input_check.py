fbbc6bf92ddc9024b3e48005378869410ff7c9a6
deronbrowne <30438804+deronbrowne@users.noreply.github.com>
2017-08-22 10:53:49 -0400

    User input check
    Defining functions that control and screen user input.

--- User_input_check.py
+++ User_input_check.py
@@ -1,7 +1,23 @@
-def check_input:
-  print '\n'.join(choice)
-  print '\n'
-  while check not in choice:
-	  print "Sorry, I didn't get that. Try again"
-	  print '\n'.join(choice)
-	  print '\n'
+def check_input(check):
+	choice=['Yes','No']
+ 	print '\n'.join(choice)
+ 	print '\n'
+ 	while check not in choice:
+		print "Sorry, I didn't get that. Try again"
+		print '\n'.join(choice)
+		print '\n'
+		check=raw_input()
+		return check
+
+
+#For edit_item_choice
+def edit_item_choice(edit):
+	options=['Name','Quantity','Price']
+	print '\n'.join(options)
+	print '\n'
+	while edit not in options:
+		print "I'm sorry, that isn't a recorded property \n"
+		print '\n'.join(options)
+		print '\n'
+		edit=raw_input()
+		return edit
