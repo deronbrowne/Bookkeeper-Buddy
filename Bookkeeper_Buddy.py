c64720f4280840ab25ca6fec664071ce240e5371
deronbrowne <dab2016@gmail.com>
2017-10-02 00:02:23 -0700

    Edited My_Projects to allow for editing items, overheads, hours and profit. Tied in MY_Projects fully with other files. Need to test and debug.

--- Bookkeeper_Buddy.py
+++ Bookkeeper_Buddy.py
@@ -15,12 +15,6 @@ pprint.pprint(Inventory.inventory_record)
 My_Projects.projects = pickle.load(pkl_file)
 pprint.pprint(My_Projects.projects)
 
-My_Projects.instances = pickle.load(pkl_file)
-pprint.pprint(My_Projects.instances)
-
-My_Projects.project_total = pickle.load(pkl_file)
-pprint.pprint(My_Projects.project_total)
-
 pkl_file.close()
 
 
@@ -149,19 +143,22 @@ while state=='on':
 
         #Create
         #######################################################################
-        while query=='create':# if user wants to create a project
-            My_Projects.projects.append(raw_input('Name your project! '))
-            My_Projects.instances.append(My_Projects.my_projects()) #my attempt at using user input to initialize an instance
-            x=len(My_Projects.instances)-1
-            My_Projects.instances[x].add_item()
+        if query=='create':# if user wants to create a project
+            new_name=raw_input('Name this inventory! \n'
+                                            '\n')        
+            My_Projects.projects[new_name]=My_Projects.my_projects() #initialize instance; want to allow user to create more than one inventory...need to assign user input to instance name       
+            names=My_Projects.my_projects.keys()
+            a=names.index(new_name)
+            My_Projects.my_projects[My_Projects.my_projects.keys()[a]].add_item() #add items
 
         #Edit
         #######################################################################
-        while query=='edit': #if user wants to edit something
-            level=5
-            prompt=('Which project are you editing')
-            input_check.check(level,prompt)
-            
+        elif query=='edit': #if user wants to edit something
+        
+            level=14
+            prompt='Which project would you like to edit?'
+            key=input_check.check(level,prompt) #saves the name of the inventory the user wants to access
+            a=My_Projects.projects.keys().index(key) #saves the index of the key the user is accessing            
             
             #asks user what kind of edit he'd like to carry out
             level=3
@@ -174,20 +171,63 @@ while state=='on':
                 level=6
                 prompt=('What are you adding to?')
                 sub_sub_query=input_check.check(level,prompt)
-                pass
                 
-            elif sub_query== 'change':
+                if sub_sub_query=='items':
+                    My_Projects.projects[key].add_item()
+                    
+                elif sub_sub_query=='overheads':
+                    My_Projects.projects[key].add_overhead_item()
+                    
+                elif sub_sub_query=='hours':
+                    My_Projects.projects[key].log_hours()
+                    
+                elif sub_sub_query=='profits':
+                    My_Projects.projects[key].set_profit()
+                    
+                elif sub_sub_query=='done':
+                    pass
+                    
+                
+            elif sub_query=='change':
                 level=6
                 prompt=('What are you changing?')
                 sub_sub_query=input_check.check(level,prompt)
-                pass
+
+                if sub_sub_query=='items':
+                    My_Projects.projects[key].edit_item()
+                    
+                elif sub_sub_query=='overheads':
+                    My_Projects.projects[key].edit_overheads()
+                    
+                elif sub_sub_query=='hours':
+                    My_Projects.projects[key].edit_hours()
+                    
+                elif sub_sub_query=='profits':
+                    My_Projects.projects[key].set_profit()
+                    
+                elif sub_sub_query=='done':
+                    pass
                                        
             elif sub_query== 'delete':
                 #asks user which project characteristic he would like to delete
                 level=6
                 prompt=('What are you deleting?')
                 sub_sub_query=input_check.check(level,prompt)
-                pass
+
+                if sub_sub_query=='items':
+                    My_Projects.projects[key].delete_items()
+                    
+                elif sub_sub_query=='overheads':
+                    My_Projects.projects[key].delete_overhead()
+                    
+                elif sub_sub_query=='hours':
+                    My_Projects.projects[key].delete_hours()
+                    
+                elif sub_sub_query=='profits':
+                    My_Projects.projects[key].delete_profit()
+                    
+                elif sub_sub_query=='done':
+                    pass
                
             elif sub_query== 'save & exit':
                 #once user is finished editing, ask user what he'd like to do next
@@ -198,76 +238,30 @@ while state=='on':
 
         #View
         #######################################################################
-        while query=='View': #ask user to identify which part of the project he'd like to view
+        while query=='view': #ask user to identify which part of the project he'd like to view
             level=6
             prompt=('What would you like to view?')
-            sub_query=input_check.check(level,prompt)
-            pass        
+            sub_sub_query=input_check.check(level,prompt)
 
-# at this point I'd like to be able to reference particular instances of the My_Projects class based on the user defined name for the class. Not sure how to do that.
-            if sub_query== 'item':
-                print 'items'
-                pass
-            
-            elif sub_query== 'overheads':
-                print 'overheads'
-                pass
-            
-            elif sub_query== 'hours':
-                print 'hours'
-                pass
-            
-            elif sub_query== 'profit':
-                print 'profit'
-                pass
-
-            elif sub_query== 'done':
+            if sub_sub_query=='items':
+                My_Projects.projects[key].view_items()
+                
+            elif sub_sub_query=='overheads':
+                My_Projects.projects[key].view_overheads()
+                
+            elif sub_sub_query=='hours':
+                My_Projects.projects[key].view_hours()
+                
+            elif sub_sub_query=='profits':
+                My_Projects.projects[key].view_profit()
+                
+            elif sub_sub_query=='done':
+                #once user is finished viewing, ask user what he'd like to do next
                 level=2
                 prompt=('What would you like to do?')
                 query=input_check.check(level,prompt)
-                pass             
-
-        #Delete
-        #######################################################################
-        while query=='delete':
-            level=6
-            prompt=('What would you like to delete?')
-            sub_query=input_check.check(level,prompt)
-            pass 
-
-            if sub_query== 'item':
-                print 'Deleting item'
-
-            elif sub_query== 'overheads':
-                print 'Deleting overheads'
-
-            elif sub_query== 'hours':
-                print 'Deleting hours'
-
-            elif sub_query== 'profit':
-                print 'Deleting profit'
-
-            elif sub_query== 'done':
-                level=2
-                prompt=('What would you like to do?')
-                query=input_check.check(level,prompt)
-                pass  
-           
-
-        #Exit without saving
-        #######################################################################
-        if query=='Exit without saving':
-            print "Write code to undo previous actions"
-            level=1
-            prompt=('Pick an option')
-            action=input_check.check(level,prompt)      
-
-        #Save & Exit
-        #######################################################################
-        elif query=='Save & Exit':
-            level=1
-            prompt=('Pick an option')
-            action=input_check.check(level,prompt)         
+                pass           
+        
                         
     while action=='Restock':
         print 'Coming soon!'
@@ -291,8 +285,6 @@ output = open('data.pkl', 'wb')
 
 # Pickle dictionary using protocol 0.
 pickle.dump(Inventory.inventory_record, output)
-pickle.dump(My_Projects.projects, output, -1)
-pickle.dump(My_Projects.instances, output, -1)
-pickle.dump(My_Projects.project_total, output, -1)
+pickle.dump(My_Projects.projects, output)
 
 output.close()
